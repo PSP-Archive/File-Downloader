92fb16783ff8271dd0fbbf787f4530bafd62e42f
korigamik <korigamik@gmail.com>
2023-01-02 12:16:16 +0530

    fix missing headers

--- setupwifi.h
+++ setupwifi.h
@@ -1,6 +1,9 @@
 #ifndef DEF_WIFI
 #define DEF_WIFI
 
+#include <cstddef>
+#include <pspkerneltypes.h>
+
 size_t write_data(void *ptr, size_t size, size_t nmemb, void *stream);
 void run_curl_test(char *addurl, char *filename);
 int connect_to_apctl(int config);
